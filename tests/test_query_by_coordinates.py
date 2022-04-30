b4fb4848ee5a68bd80db9cc4f85b91a79361c04e
ScottL <sjlteacher@gmail.com>
2022-04-30 13:26:38 -0400

    Test by zipcode verifies name corresponds to city

--- tests/test_query_by_coordinates.py
+++ tests/test_query_by_coordinates.py
@@ -2,7 +2,10 @@ import pytest
 from helpers import get_current_weather
 
 
-@pytest.mark.parametrize("coordinates", [{"lat": 35, "lon": 139}, {"lat": -16, "lon": 145}])
+coordinates = [{"lat": 35, "lon": 139}, {"lat": -16, "lon": 145}]
+
+
+@pytest.mark.parametrize("coordinates", coordinates)
 def test_query_by_coordinates(base_url: str, api_key: str, coordinates: dict):
     formatted_coordinates = f"lat={coordinates['lat']}&lon={coordinates['lon']}"
     status_code, response = get_current_weather(base_url=base_url, api_key=api_key, query_params=formatted_coordinates)
---
+++ tests/test_query_by_zip_code.py
@@ -0,0 +1,21 @@
+import pytest
+from helpers import get_current_weather
+
+
+cities = [
+    {"name": "Mountain View", "zipcode": "94040"},
+    {"name": "New York", "zipcode": "10007"},
+    {"name": "New York", "zipcode": "10007", "country": "us"},
+    {"name": "Mountain View", "zipcode": "94040"},
+]
+
+
+@pytest.mark.parametrize("city", cities)
+def test_query_by_zip_code(base_url: str, api_key: str, city: dict):
+    formatted_city = f"zip={city['zipcode']}"
+    if city.get("country"):
+        formatted_city += f",{city['country']}"
+        
+    status_code, response = get_current_weather(base_url=base_url, api_key=api_key, query_params=formatted_city)
+    assert status_code == 200
+    assert response["name"] == city["name"]
