5571ee031d9b2c82597518cea05b5ad80929057f
ScottL <sjlteacher@gmail.com>
2022-05-02 13:55:24 -0400

    Tests different combinations of parameters and edge cases

--- tests/test_query_by_city_name.py
+++ tests/test_query_by_city_name.py
@@ -5,16 +5,32 @@ from helpers import get_current_weather
 cities = [
     {"name": "London", "query": ["London"]},
     {"name": "London", "query": ["London", "uk"]},
-    {"name": "New York", "query": ["New York"]},
-    {"name": "New York", "query": ["New York", "ny"]},
+    {"name": "New York", "query": ["New York City"]},
+    {"name": "New York", "query": ["New York City", "ny"]},
     {"name": "New York", "query": ["New York", "ny", "us"]},
     {"name": "New York", "query": ["New York", "us"]},
+    {"name": "Tokyo", "query": ["Tokyo"]},
+    {"name": "Tokyo", "query": ["Tokyo", "jp"]},
+    {"name": "Shanghai", "query": ["Shanghai"]},
+    {"name": "Shanghai", "query": ["Shanghai", "cn"]},
+    {"name": "Mexico City", "query": ["Mexico City"]},
+    {"name": "Mexico City", "query": ["Mexico City", "mx"]},
+    {"name": "Truth or Consequences", "query": ["Truth or Consequences"]},
+    {"name": "Truth or Consequences", "query": ["Truth or Consequences", "us"]},
+    {
+        "name": "Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch",
+        "query": ["Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch"],
+    },
+    {
+        "name": "Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch",
+        "query": ["Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch", "gb"],
+    },
 ]
 
 
-@pytest.mark.parametrize("city", cities)
+@pytest.mark.parametrize("city", cities, ids=[city["name"] for city in cities])
 def test_query_by_city_name(base_url: str, api_key: str, city: dict):
     formatted_city = f"q={','.join(city['query'])}"
     status_code, response = get_current_weather(base_url=base_url, api_key=api_key, query_params=formatted_city)
-    assert status_code == 200
-    assert response["name"] == city["name"]
+    assert status_code == 200, f"\nTest data: {city}\nResponse: {response}"
+    assert response["name"] == city["name"], f"Expected: {city['name']}, Actual: {response['name']}"
--- tests/test_query_by_coordinates.py
+++ tests/test_query_by_coordinates.py
@@ -2,13 +2,30 @@ import pytest
 
 from helpers import get_current_weather
 
-coordinates = [{"lat": 35, "lon": 139}, {"lat": -16, "lon": 145}]
+coordinates = [
+    {"lat": 35, "lon": 139},
+    {"lat": -16, "lon": 145},
+    {"lat": -33, "lon": -70},
+    {"lat": 40, "lon": -74},
+    {"lat": -90, "lon": -180},
+    {"lat": 90, "lon": 180},
+    {"lat": 0, "lon": 0},
+    {"lat": 51, "lon": 0},
+    {"lat": 51.5, "lon": 0.1},
+    {"lat": 51.51, "lon": 0.12},
+    {"lat": 51.507, "lon": 0.127},
+    {"lat": 51.5072, "lon": 0.1276},
+]
 
 
-@pytest.mark.parametrize("coordinates", coordinates)
+@pytest.mark.parametrize("coordinates", coordinates, ids=[f"{item['lat']},{item['lon']}" for item in coordinates])
 def test_query_by_coordinates(base_url: str, api_key: str, coordinates: dict):
     formatted_coordinates = f"lat={coordinates['lat']}&lon={coordinates['lon']}"
     status_code, response = get_current_weather(base_url=base_url, api_key=api_key, query_params=formatted_coordinates)
-    assert status_code == 200
-    assert response["coord"]["lat"] == coordinates["lat"]
-    assert response["coord"]["lon"] == coordinates["lon"]
+    assert status_code == 200, f"\nTest data: {coordinates}\nResponse: {response}"
+    assert (
+        response["coord"]["lat"] == coordinates["lat"]
+    ), f"Expected: {coordinates['lat']}, Actual: {response['coord']['lat']}"
+    assert (
+        response["coord"]["lon"] == coordinates["lon"]
+    ), f"Expected: {coordinates['lon']}, Actual: {response['coord']['lon']}"
